e5f180ce01bed19a536f4ba1dc91eafdc750cd87
AfifurRaihan <113502491+AfifurRaihan@users.noreply.github.com>
2022-09-27 07:36:24 -0700

    Add files via upload

---
+++ src/test/java/com/afifur/latihan2/services/Latihan2ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afifur.latihan2.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
